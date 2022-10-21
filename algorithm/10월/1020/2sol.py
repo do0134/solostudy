015e85085a45cb7615406c55ef6dbb14cd178ea5
Tony Woo <do0134@Naver.com>
2022-10-22 00:31:00 +0900

    1021 1sol

--- algorithm/10월/1020/2sol.py
+++ algorithm/10월/1020/2sol.py
@@ -1,8 +1,23 @@
 import heapq as h
 
+def Dik(start, end):
+    q = list()
+    v = [int(1e9)]*(n+1)
+    v[start] = 0
+    h.heappush(q,(start,0))
+    while q:
+        cc, cw = h.heappop(q)
+        if v[cc] < cw:
+            pass
+        else:
+            for nc,nw in tree[cc]:
+                if v[nc] >= v[cc]+nw:
+                    v[nc] = v[cc]+nw
+                    h.heappush(q,(nc,v[nc]))
+    return v[end]
+
 n,m = map(int,input().split())
 tree = [[] for _ in range(n+1)]
-v = [[0]*(n+1) for _ in range(n+1)]
 for _ in range(m):
     s,e,w = map(int,input().split())
     tree[s].append((e,w))
@@ -10,7 +25,11 @@ for _ in range(m):
 
 v1,v2 = map(int,input().split())
 
-q = list()
-for i in tree[1]:
-    h.heappush(q,(i[0],i[1]))
+a = Dik(1,v1) + Dik(v1,v2) +Dik(v2,n)
+b = Dik(1,v2) + Dik(v2,v1) + Dik(v1,n)
+
+if min(a,b) >= int(1e9):
+    print(-1)
+else:
+    print(min(a,b))
 
