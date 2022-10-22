8812cf3ce0b7352136af7fb67cee6d7e3fa59bc6
TonyWoo <do0134@naver.com>
2022-10-23 01:41:16 +0900

    1022 develop business logic

---
+++ spring/1020-spring+jpa/jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.controller;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberForm {
+
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}
