bf6570ad071ff73ebc784dc554344c1b6102de00
Tony Woo <do0134@Naver.com>
2022-12-03 22:19:19 +0900

    leetcode 451. Sort Characters By Frequency

---
+++ algorithm/12월/1203/1sol.py
@@ -0,0 +1,12 @@
+# leetcode 451. Sort Characters By Frequency
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        s_set = set(s)
+        cnt = sorted(Counter(s).items(), key=lambda item: item[1], reverse=True)
+
+        answer = ""
+        for k, v in cnt:
+            answer += k * v
+
+        return answer
