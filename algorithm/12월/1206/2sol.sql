39e618e7ca84db93a0d441194a5555e14dd3fb3b
Tony Woo <do0134@Naver.com>
2022-12-06 21:57:04 +0900

    leetcode 177. Nth Highest Salary

--- algorithm/12월/1206/2sol.sql
+++ algorithm/12월/1206/2sol.sql
@@ -5,7 +5,7 @@ BEGIN
 
 SET N=N-1;
   RETURN (
-      # Write your MySQL query statement below.
+      -- Write your MySQL query statement below.
       SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET N
   );
 END
