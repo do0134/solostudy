df3b63effd53a5b31afa6805baccc7da614743a5
Tony Woo <do0134@Naver.com>
2022-12-09 23:55:47 +0900

    leetcode 181. Employees Earning More Than Their Managers

---
+++ algorithm/12월/1209/2sol.sql
@@ -0,0 +1,3 @@
+-- leetcode 181. Employees Earning More Than Their Managers
+-- Write your MySQL query statement below
+select e1.name as Employee from Employee e1, Employee e2 where e1.managerId = e2.id and e1.salary > e2.salary 
