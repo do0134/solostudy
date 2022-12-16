10e9881c0e20b2a45c5728e815470765e3dbc28d
Tony Woo <do0134@Naver.com>
2022-12-16 22:44:37 +0900

    leetcode 232. Implement Queue using Stacks

---
+++ algorithm/12월/1217/1sol.py
@@ -0,0 +1,33 @@
+# leetcode 232. Implement Queue using Stacks
+
+class MyQueue:
+
+    def __init__(self):
+        self.q = deque()
+        
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        
+
+    def pop(self) -> int:
+        return self.q.popleft()
+        
+
+    def peek(self) -> int:
+        return self.q[0]
+        
+
+    def empty(self) -> bool:
+        if not self.q :
+            return True
+        else : 
+            return False
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
