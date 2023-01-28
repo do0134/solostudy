e58456e3a5c1ec50db9e2b671ca96fab3fc87070
Tony Woo <do0134@Naver.com>
2023-01-29 00:52:39 +0900

    leetcode 472. Concatenated Words

--- algorithm/1월/0127/1sol.py
+++ algorithm/1월/0127/1sol.py
@@ -2,17 +2,21 @@
 # https://leetcode.com/problems/concatenated-words/
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        def word_in_list(target_word, word_arr):
+            if target_word in word_arr:
+                return True
+
+            for i in range(len(target_word), 0, -1):
+                if target_word[:i] in word_arr and word_in_list(target_word[i:], word_arr):
+                    return True
+            return False
+
         answer = list()
         words_set = set(words)
-        for word in words :
-            check = 0
+
+        for word in words:
             words_set.remove(word)
-            for i in range(len(word),0,-1) :
-                if word[:i] in words_set :
-                    print(word, word[:i])
-                    check += 1
-                if check == 2 :
-                    answer.append(word)
-                    break
+            if word_in_list(word, words_set):
+                answer.append(word)
             words_set.add(word)
         return answer
