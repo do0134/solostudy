cddd98af23ca03a3c14ddaafa03a745b3f86fe67
do0134 <do0134@naver.com>
2023-06-28 22:35:06 +0900

    spring-oop이해 및 실습

---
+++ practice_project/oop-practice/src/test/java/org/example/PasswordValidatorTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+/**
+ * 비밀번호는 최소 8자 ~ 최대 12자여야 한다.
+ * 비밀번호가 8자 미만 12자 초과일 경우 IllegalArgumentException
+ * 경계조건에 대해 테스트코드 작성해야 한다.
+ */
+public class PasswordValidatorTest {
+    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
+    @Test
+    void validatePasswordTest() {
+        assertThatCode(() -> PasswordValidator.validate("serverwizard")).doesNotThrowAnyException();
+    }
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 에러가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1234567", "abcdefghijklm"})
+    void validatePasswordTest2(String password) {
+        assertThatCode(() -> PasswordValidator.validate(password)).
+                isInstanceOf(IllegalArgumentException.class).
+                hasMessage("비밀번호는 최소 8자 이상, 12자 이하여야 한다.");
+    }
+}
