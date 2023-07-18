31a6dd7c0a806a582ff970d2a413d6561806888e
do013 <do0134@naver.com>
2023-07-18 14:32:52 +0900

    백준 2812 크게 만들기

--- algorithm/7월/0718/1sol.py
+++ algorithm/7월/0718/1sol.py
@@ -1,1 +1,19 @@
-print(11).
+# 백준 2812 크게 만들기
+# https://www.acmicpc.net/problem/2812
+
+import sys
+
+n,k = map(int,input().split())
+s = sys.stdin.readline().rstrip()
+answer = list()
+
+for i in s:
+    while answer and k > 0 and answer[-1] < i:
+        answer.pop()
+        k -= 1
+    answer.append(i)
+while k > 0:
+    answer.pop()
+    k -= 1
+
+print("".join(answer))
