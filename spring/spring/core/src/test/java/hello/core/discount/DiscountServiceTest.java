1e891ee6a2b7aede7bce1af907276182dbeb1c68
do013 <do0134@naver.com>
2023-09-12 01:25:37 +0900

    spring 강의

---
+++ spring/spring/core/src/test/java/hello/core/discount/DiscountServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DiscountServiceTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void vip_o() {
+        Member member = new Member(1L, "memberVIP", Grade.VIP );
+
+        int discount = discountPolicy.discount(member,10000);
+
+        Assertions.assertThat(discount).isEqualTo(1000);
+
+    }
+    
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void vip_x() {
+        Member member = new Member(1L, "memberBasic", Grade.Normal );
+
+        int discount = discountPolicy.discount(member,10000);
+
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+}
