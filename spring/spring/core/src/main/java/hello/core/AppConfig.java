5eea69ca54f53eaba1404e5036c1621e0f8d39bc
do013 <do0134@naver.com>
2023-09-13 00:10:58 +0900

    error테스트

--- spring/spring/core/src/main/java/hello/core/AppConfig.java
+++ spring/spring/core/src/main/java/hello/core/AppConfig.java
@@ -1,20 +1,40 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    @Bean
+    public MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    
 }
