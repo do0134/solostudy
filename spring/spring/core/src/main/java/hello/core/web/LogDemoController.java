8aa382bf0d439e5142625be7970490ba79b04584
do013 <do0134@naver.com>
2023-09-24 00:26:52 +0900

    Bean Scope

---
+++ spring/spring/core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.inject.Provider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        System.out.println("myLogger = " + myLogger.getClass());
+        String requestURL = request.getRequestURI().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("여기는 컨트롤러");
+        logDemoService.logic("testId");
+        return null;
+    }
+}
