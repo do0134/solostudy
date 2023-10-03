2249b303bd5fb9f6072dcefed4a94b95ad788d60
do013 <do0134@naver.com>
2023-10-03 11:45:22 +0900

    프로그래머스 가장 긴 팰린드롬

---
+++ algorithm/10월/1003/1sol.py
@@ -0,0 +1,37 @@
+#
+
+from collections import defaultdict
+
+
+def dfs(now, v, trip, use_ticket):
+    global airlines, l, answer
+    if len(trip) == l+1:
+        answer.append(trip)
+        return
+
+    for i in airlines[now]:
+        if v[i] and use_ticket[(now,i)]:
+            v[i] -= 1
+            use_ticket[(now,i)] -= 1
+            dfs(i,v,trip+[i], use_ticket)
+            use_ticket[(now,i)] += 1
+            v[i] += 1
+
+
+def solution(tickets):
+    global airlines, l, answer
+
+    l = len(tickets)
+    airlines = defaultdict(set)
+    visit = defaultdict(int)
+    used_ticket = defaultdict(int)
+
+    for s,e in tickets:
+        airlines[s].add(e)
+        visit[e] += 1
+        used_ticket[(s,e)] += 1
+
+    answer = []
+    dfs("ICN", visit, ["ICN"], used_ticket)
+    answer.sort()
+    return answer[0]
