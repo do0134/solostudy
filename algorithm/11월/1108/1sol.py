8e19e02a7be4d0937fcadadb7274af5858efab8f
do013 <do0134@naver.com>
2023-11-08 11:10:29 +0900

    프로그래머스 디스크 컨트롤러

---
+++ algorithm/11월/1108/1sol.py
@@ -0,0 +1,35 @@
+# 프로그래머스 디스크 컨트롤러
+# https://school.programmers.co.kr/learn/courses/30/lessons/42627
+
+import heapq as hq
+
+def solution(jobs):
+    answer = 0
+    n = len(jobs)
+    hq.heapify(jobs)
+    time = 0
+    heap = list()
+
+    while jobs:
+        s, e = hq.heappop(jobs)
+        if s > time:
+            if heap:
+                hq.heappush(jobs, [s, e])
+
+            else:
+                time = s
+                hq.heappush(heap, [e, s])
+
+            next_t, next_s = hq.heappop(heap)
+            time += next_t
+            answer += time - next_s
+            continue
+        else:
+            hq.heappush(heap, [e, s])
+
+    while heap:
+        e, s = hq.heappop(heap)
+        time += e
+        answer += time - s
+
+    return answer // n
