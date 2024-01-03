ddbd63d06fa48b5fecc2b9817e60f7b1ab45f04a
do013 <do0134@naver.com>
2024-01-03 15:07:40 +0900

    https://leetcode.com/problems/array-partition

---
+++ algorithm/1월/0103/4sol_Array_Partition.py
@@ -0,0 +1,11 @@
+# Leetcode 561. Array Partition
+# https://leetcode.com/problems/array-partition
+
+class Solution:
+    def arrayPairSum(self, nums: list[int]) -> int:
+        nums.sort()
+        answer = 0
+        for i in range(0,len(nums),2):
+            answer += nums[i]
+
+        return answer
