3119f2dbc533fed9128aaaec854904f62defa3cb
do013 <do0134@naver.com>
2024-01-08 11:07:47 +0900

    백준 5397 키로거

---
+++ algorithm/1월/0108/4sol_키로거.py
@@ -0,0 +1,32 @@
+# 백준 5397 키로거
+
+from collections import deque
+
+
+def solve(s):
+    left = deque()
+    right = deque()
+
+    for i in s:
+        if i == "<":
+            if left:
+                val = left.pop()
+                right.appendleft(val)
+        elif i == ">":
+            if right:
+                val = right.popleft()
+                left.append(val)
+        elif i == "-":
+            if left:
+                left.pop()
+        else:
+            left.append(i)
+
+    return list(left+right)
+
+
+n = int(input())
+
+for _ in range(n):
+    s = list(input())
+    print("".join(solve(s)))
