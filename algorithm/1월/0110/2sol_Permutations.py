3296c13066a9434d7a449d5b01007acb109d85de
do013 <do0134@naver.com>
2024-01-10 10:09:45 +0900

    Leetcode 46. Permutations

--- algorithm/1월/0110/2sol_Permutations.py
+++ algorithm/1월/0110/2sol_Permutations.py
@@ -3,9 +3,30 @@
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        answer = list()
 
-        for i in permutations(nums, len(nums)):
-            answer.append(i)
+        # for i in permutations(nums, len(nums)):
+        #     answer.append(i)
+
+        # return answer
+
+        q = deque()
+        n = len(nums)
+
+        for i in range(n):
+            temp = set()
+            temp.add(i)
+            q.append(([nums[i]], temp))
+
+        while q:
+            now, v = q.popleft()
+
+            if len(v) >= n:
+                answer.append(now)
+            else:
+                for i in range(n):
+                    if i not in v:
+                        v.add(i)
+                        q.append((now + [nums[i]], copy.deepcopy(v)))
+                        v.remove(i)
 
         return answer
