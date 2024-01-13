a7692ef51ec1861c7dc6eac9dc52252d8f9b9839
do013 <do0134@naver.com>
2024-01-13 14:32:15 +0900

    백준 13905 세부

---
+++ algorithm/1월/0113/2sol_세부.py
@@ -0,0 +1,34 @@
+# 백준 13905 세부
+# https://www.acmicpc.net/problem/13905
+
+import sys
+import heapq as hq
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+S,E = map(int,input().split())
+
+island = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    h1,h2,k = map(int,input().split())
+    island[h1].append((-k, h2))
+    island[h2].append((-k, h1))
+
+v = [0]*(N+1)
+heap = list()
+v[S] = 0
+
+hq.heappush(heap, (-int(1e9), S))
+answer = 0
+
+while heap:
+    min_w, now_idx = hq.heappop(heap)
+
+    for next_w, next_idx in island[now_idx]:
+        weight = max(next_w, min_w)
+        if v[next_idx] > weight:
+            v[next_idx] = weight
+            hq.heappush(heap, (weight, next_idx))
+
+print(-v[E])
