82898a6dbe6d0e8768c2d9f7e52586ae0df1161e
do013 <do0134@naver.com>
2024-01-13 15:49:46 +0900

    백준 1647 도시 분할 계획

--- algorithm/1월/0113/3sol_도시분할계획.py
+++ algorithm/1월/0113/3sol_도시분할계획.py
@@ -1,17 +1,19 @@
+# 백준 1647 도시 분할 계획
+# https://www.acmicpc.net/problem/1647
+
 import sys
+
 input = sys.stdin.readline
 
 n, m = map(int,input().split())
-cities = [[] for _ in range(n+1)]
+cities = list()
 
 for _ in range(m):
     s,e,w = map(int,input().split())
-    cities[s].append((w,e))
-    cities[e].append((w,s))
+    cities.append((w,s,e))
 
-
+cities.sort()
 parent = [i for i in range(n+1)]
-rank = [i for i in range(n+1)]
 
 
 def find(x):
@@ -25,4 +27,19 @@ def union(a,b):
     x = find(a)
     y = find(b)
 
+    if x < y:
+        parent[x] = y
+    else:
+        parent[y] = x
 
+
+answer = 0
+delete_cost = 0
+
+for cw, cs, ce in cities:
+    if find(cs) != find(ce):
+        union(cs, ce)
+        answer += cw
+        delete_cost = cw
+
+print(answer-delete_cost)
