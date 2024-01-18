6764561f7dd618dd1c2f25054d1d54a20f4baba6
do013 <do0134@naver.com>
2024-01-18 09:15:28 +0900

    백준 13305 주유소

--- algorithm/1월/0118/2sol.py
+++ algorithm/1월/0118/2sol.py
@@ -8,8 +8,7 @@ answer = cost[0]*road[0]
 min_v = cost[0]
 
 for i in range(1,n-1):
-    if cost[i] < min_v:
-        min_v = cost[i]
+    min_v = min(min_v, cost[i])
     answer += min_v*road[i]
 
 print(answer)
