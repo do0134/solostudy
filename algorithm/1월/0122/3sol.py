285cd6262e933a581a54d66f621f32b0f04220a6
do013 <do0134@naver.com>
2024-01-22 10:12:57 +0900

    Leetcode 198. House Robber

---
+++ algorithm/1월/0122/3sol.py
@@ -0,0 +1,16 @@
+# Leetcode 198. House Robber
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+
+        if n == 1:
+            return nums[0]
+
+        dp[0] = nums[0]
+
+        for i in range(n - 1):
+            dp[i + 1] = max(dp[i], nums[i + 1] + dp[i - 1])
+
+        return max(dp)
