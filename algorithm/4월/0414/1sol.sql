c2697c1eb5820bf014c6247cad414261cdbbea2d
do013 <do0134@naver.com>
2024-04-14 22:02:59 +0900

    hackerrank Type of Triangle

---
+++ algorithm/4월/0414/1sol.sql
@@ -0,0 +1,11 @@
+-- hackerrank Type of Triangle
+
+SELECT
+    CASE
+        WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+        WHEN A = B AND B = C THEN 'Equilateral'
+        WHEN A = B OR A = C OR B = C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END AS triangle_type
+FROM
+    triangles;
