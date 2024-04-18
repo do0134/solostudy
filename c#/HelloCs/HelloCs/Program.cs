e8819e1e834d60ad0aaaa8adac51e188e87869d8
do013 <do0134@naver.com>
2024-04-18 17:20:33 +0900

    hello c#

---
+++ c#/HelloCs/HelloCs/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, C#!");
+        Console.WriteLine("Hello, " + args[0]);
+        Console.ReadKey();
+    }
+}
