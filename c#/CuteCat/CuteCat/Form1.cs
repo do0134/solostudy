9078c5d5b115c6405fcec765ef92a3a0f8cb9ebf
do013 <do0134@naver.com>
2024-04-24 23:05:11 +0900

    클래스 예시

---
+++ c#/CuteCat/CuteCat/Form1.cs
@@ -0,0 +1,27 @@
+namespace CuteCat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Cat
+    {
+        string Name;
+        int Age;
+        int Happiness;
+
+        void Play()
+        {
+            Console.WriteLine("³Ä¿Ë");
+        }
+    }
+}
