3b1b15e99249680b98d46e5d49ab5aed6bda0112
Tony Woo <do0134@naver.com>
2024-12-23 16:45:21 +0900

    14651 걷다보니 신천역 삼

---
+++ algorithm/12월/1223/1sol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+// 14651 걷다보니 신천역 삼
+namespace Algo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long answer = 2;
+            if (n == 1)
+                answer = 0;
+            else if (n == 2)
+                answer = 2;
+            else
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    answer *= 3;
+                    answer %= 1000000009;
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
